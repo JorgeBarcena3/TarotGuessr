3d38bfb7c5e7b0be1db943172d49a336a4ddbcba
Jorge <j.barcenalumbreras@gmail.com>
2023-09-03 00:23:14 +0200

    Logica base

---
+++ config/config.js
@@ -0,0 +1,136 @@
+var IMAGE_PATH = "../images/";
+var MAX_ANSWERS = 4;
+var CONFIG = [
+    {
+        "Name": "The Magician",
+        "Value": 1,
+        "Image": "1.MAGO.png",
+        "Description": "Poder, habilidad, manifestación"
+    },
+    {
+        "Name": "SACERDOTISA",
+        "Value": 2,
+        "Image": "2.SACERDOTISA.png",
+        "Description": "Intuición, secreto, misterio"
+    },
+    {
+        "Name": "EMPERATRIZ",
+        "Value": 3,
+        "Image": "3.EMPERATRIZ.png",
+        "Description": "Fertilidad, creatividad, abundancia"
+    },
+    {
+        "Name": "EMPERADOR",
+        "Value": 4,
+        "Image": "4.EMPERADOR.png",
+        "Description": "Autoridad, liderazgo, control"
+    },
+    {
+        "Name": "PAPA",
+        "Value": 5,
+        "Image": "5.PAPA.png",
+        "Description": "Espiritualidad, tradición, conocimiento"
+    },
+    {
+        "Name": "ENAMORADOS",
+        "Value": 6,
+        "Image": "6.ENAMORADOS.png",
+        "Description": "Elección, amor, dilema"
+    },
+    {
+        "Name": "CARRO",
+        "Value": 7,
+        "Image": "7.CARRO.png",
+        "Description": "Determinación, avance, control"
+    },
+    {
+        "Name": "JUSTICIA",
+        "Value": 8,
+        "Image": "8.JUSTICIA.png",
+        "Description": "Equilibrio, imparcialidad, karma"
+    },
+    {
+        "Name": "HERMITANO",
+        "Value": 9,
+        "Image": "9.HERMITANO.png",
+        "Description": "Reflexión, soledad, sabiduría"
+    },
+    {
+        "Name": "RUEDA",
+        "Value": 10,
+        "Image": "10.RUEDA.png",
+        "Description": "Cambio, destino, oportunidad"
+    },
+    {
+        "Name": "FUERZA",
+        "Value": 11,
+        "Image": "11.FUERZA.png",
+        "Description": "Coraje, dominio, paciencia"
+    },
+    {
+        "Name": "COLGADO",
+        "Value": 12,
+        "Image": "12.COLGADO.png",
+        "Description": "Sacrificio, liberación, perspectiva"
+    },
+    {
+        "Name": "MUERTE",
+        "Value": 13,
+        "Image": "13.MUERTE.png",
+        "Description": "Transformación, renacimiento, final"
+    },
+    {
+        "Name": "TEMPLANZA",
+        "Value": 14,
+        "Image": "14.TEMPLANZA.png",
+        "Description": "Equilibrio, armonía, moderación"
+    },
+    {
+        "Name": "DIABLO",
+        "Value": 15,
+        "Image": "15.DIABLO.png",
+        "Description": "Tentación, adicción, ataduras"
+    },
+    {
+        "Name": "TORRE",
+        "Value": 16,
+        "Image": "16.TORRE.png",
+        "Description": "Cambio abrupto, destrucción, revelación"
+    },
+    {
+        "Name": "ESTRELLA",
+        "Value": 17,
+        "Image": "17.ESTRELLA.png",
+        "Description": "Esperanza, inspiración, guía"
+    },
+    {
+        "Name": "LUNA",
+        "Value": 18,
+        "Image": "18.LUNA.png",
+        "Description": "Ilusiones, emociones, intuición"
+    },
+    {
+        "Name": "SOL",
+        "Value": 19,
+        "Image": "19.SOL.png",
+        "Description": "Alegría, éxito, claridad"
+    },
+    {
+        "Name": "JUICIO",
+        "Value": 20,
+        "Image": "20.JUICIO.png",
+        "Description": "Evaluación, despertar, resolución"
+    },
+    {
+        "Name": "MUNDO",
+        "Value": 21,
+        "Image": "21.MUNDO.png",
+        "Description": "Logro, completitud, realización"
+    },
+    {
+        "Name": "LOCO",
+        "Value": 22,
+        "Image": "22.LOCO.png",
+        "Description": "Espontaneidad, aventura, riesgo"
+    }
+]
---
+++ scripts/tarotGuesr.js
@@ -0,0 +1,158 @@
+
+let mPoints = 0;
+let currentCard = {};
+let isGaming = true;
+
+// MAX y MIN INCLUSSIVE
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let ClickSound;
+let GoodSound;
+let WrongSound;
+
+function createSounds() {
+
+    ClickSound = document.createElement('audio');
+    ClickSound.setAttribute('src', '../Sounds/click.mp3');
+
+    GoodSound = document.createElement('audio');
+    GoodSound.setAttribute('src', '../Sounds/Good.mp3');
+
+    WrongSound = document.createElement('audio');
+    WrongSound.setAttribute('src', '../Sounds/Wrong.mp3');
+
+    Deselect = document.createElement('audio');
+    Deselect.setAttribute('src', '../Sounds/deselect.mp3');
+
+}
+
+function revelateResponse(response) {
+    $(".initialContainerAnwser").each(function () {
+        let optionResponse = $(this)[0].innerText;
+
+        if (optionResponse == currentCard.Description) {
+            $(this).css("background", "green");
+        }
+
+    });
+}
+
+function checkResponse(response) {
+    if (response == currentCard.Description) {
+        mPoints++;
+    }
+    else {
+        mPoints--;
+    };
+
+    $('#points').text(mPoints);
+
+    return response == currentCard.Description;
+}
+
+function generateResponse(cardIndex) {
+    let responses = [];
+    responses.push(CONFIG[cardIndex].Description);
+
+    let CardsResponse = [...CONFIG];
+    CardsResponse.splice(cardIndex, 1);
+
+    for (let index = 0; index < MAX_ANSWERS - 1; index++) {
+        let randomIndex = 0;
+
+        do {
+            randomIndex = getRandomNumber(0, CardsResponse.length - 1);
+        } while (randomIndex == cardIndex);
+
+        responses.push(CardsResponse[randomIndex].Description);
+        CardsResponse.splice(randomIndex, 1);
+    }
+
+    let responsesLabel = ["1", "2", "3", "4"];
+    for (let index = 0; index < MAX_ANSWERS + 1; index++) {
+        let randomIndex = getRandomNumber(0, responses.length - 1);
+        let randomLabel = getRandomNumber(0, responsesLabel.length - 1);
+        $('#initialContainerAnwser' + responsesLabel[randomLabel]).text(responses[randomIndex]);
+
+        responsesLabel.splice(randomLabel, 1);
+        responses.splice(randomIndex, 1);
+
+    }
+    $(".initialContainerAnwser").css("background", "#042d47");
+    isGaming = true;
+
+    console.log(responses);
+
+}
+
+function changeCard() {
+    let randomValue = 0;
+    do {
+        randomValue = getRandomNumber(0, CONFIG.length - 1);
+    } while (currentCard.Name == CONFIG[randomValue].Name);
+    currentCard = CONFIG[randomValue];
+    generateResponse(randomValue);
+    $('#initialContainerCardImage').attr('src', IMAGE_PATH + currentCard.Image);
+    $('#initialContainerNameTitle').text(currentCard.Name);
+
+    console.log("Random Value: " + randomValue);
+    console.log("Card: " + JSON.stringify(currentCard));
+}
+
+$(document).ready(function () {
+
+    changeCard();
+    createSounds();
+
+    $("#initialContainerNamePoints").on("click", function () {
+        $('#points').text(0);
+        mPoints = 0;
+    });
+
+    $("#initialContainerCardImage").on("click", function () {
+        changeCard();
+    });
+
+    let oldResponse = "";
+    $(".initialContainerAnwser").on("click", function (e) {
+
+        if (!isGaming) {
+            changeCard();
+            return;
+        }
+
+        let newResponse = $(this)[0].innerText;
+        if (newResponse == oldResponse) // Two clicks
+        {
+            if (checkResponse(e.target.innerText)) {
+                $(this).css("background", "green");
+                GoodSound.play();
+            }
+            else {
+                $(this).css("background", "red");
+                WrongSound.play();
+            };
+
+            revelateResponse(newResponse);
+            isGaming = false;
+            oldResponse = "";
+
+        }
+        else if (oldResponse == "") // First click
+        {
+            $(this).css("background", "rgb(18 91 137)");
+            oldResponse = newResponse;
+            ClickSound.play();
+
+        }
+        else {
+            $(".initialContainerAnwser").css("background", "#042d47");
+            oldResponse = "";
+            Deselect.play();
+        }
+
+    });
+
+});
